d5e24be007cab5c1049dd2d64cd320551c4cfc8b
Tobias Worledge <tobiasworledge@gmail.com>
2021-11-30 00:46:07 -0800

    Finished

--- contest.scm
+++ contest.scm
@@ -7,45 +7,101 @@
 ;;; Description:
 ;;;   brrrrrrrrrr
 ;;;   Each pixel is 1 meter
-;;;    
-(define fps 100) ;Frames per second
-(define gravity -9.81) ;y_acc due to gravity
-(define elasticity -0.8)
-(define colors (cons "black" (cons "red" (cons "blue" (cons "green" (cons "orange" (cons "yellow" (cons "purple" (cons "red" nil)))))))))
-(define (frame cf x y x_vel y_vel x_acc y_acc bounce colors x_start y_start)
-  (setposition x y)
-  ;(pd)
-  (pixel x y (car colors))
-  ;(pu)
-  (if (< (abs (modulo x 100)) 0.01) ;Displaying info
-    (display x y cf (newline))
-  )
-  (define x (+ x (/ x_vel fps)))
-  (define y (+ y (/ y_vel fps)))
-  (cond ;Bounce
-    ((and (and (< x 100) (<= y 0)) (< y_vel 0))
-      (define y_vel (* y_vel bounce)))
+;;;
+
+(define (min a b) (if (< a b) a b))
+
+(define (dot x y theta color)
+  (pixel (* x (sin (* theta 3.141592653589793238462643383279502884))) (+ y (/ (* x (cos (* theta 3.141592653589793238462643383279502884))) 5)) color) ;; convert theta to rad
+)
+
+(define (bounce x y yv c t)
+  (cond
+    ((< yv 0)
+      (cond
+        ((and (<= x 100) (< y 300)) ;First circle is 100 radius, y 300
+          (* yv -0.5))
+        ((and (and (< (abs x) 200) (> (abs x) 100)) (< y 100)) ;Second circle is 200 radius, y 100
+          (* yv -0.5))
+        (else ;No bounce, yv stays the same
+          (dot x y t c)
+          (+ yv -0.981)
+        ) 
+      )
+    )
     (else
-      (define y_vel (+ y_vel (/ y_acc fps))))
+      (dot x y t c)
+      (+ yv -0.981)
+    )
   )
-  
+)
+
+; cf => current frame
+; x => x coordinate
+; y => y coordinate
+; yv => y velocity
+; xs => x starting coordinate
+; ys => y starting coordinate
+; c => current color
+; seed => current seed for rng
+; t => theta of current ball
+(define (frame cf x y yv xs ys seed t)
+  (define x (+ x 1)) ;Updating x
+  (define y (+ y (/ yv 10))) ;Updating y
   (cond
     ((and (>= y -500) (<= x 500))
-      (frame (+ cf 1) x y x_vel y_vel x_acc y_acc bounce colors x_start y_start))
-    ((> y_start 300)
-      (color (car (cdr colors)))
-      (frame 0 -500 (- y_start 25) x_vel 0 0 gravity bounce (cdr colors) x_start (- y_start 25)))
+      (define c
+        (rgb 
+          (- 1 (/ x 350))
+          (min 1 (- 1 (/ (abs yv) 200)))
+          (- 1 (/ (+ y 500) 1000))
+        )
+      )
+      (frame (+ cf 1) x y (bounce x y yv c t) xs ys seed t))
+    ((<= (abs t) 1) ;New ball
+      (cond ;DETERMINE IF FINISHED THIS COLUMN
+        ((<= ys 400) ;Finished column
+          (cond
+            ((<= t 0)
+              (define t (+ (* t -1) 0.002)))
+            (else
+              (define t (* t -1)))
+          )
+          (frame 0 0 450 0 0 450 seed t)
+        )
+        (else
+          (define start_y (- ys 10))
+          (frame 0 0 start_y 0 0 start_y seed t)
+        )
+      )
+    )
+  )
+)
+
+(define (platforms t y radius)
+  (goto (* radius (sin (* t 3.141592653589793238462643383279502884))) (+ y (/ (* radius (cos (* t 3.141592653589793238462643383279502884))) 5)))
+  (cond
+    ((>= t 2)
+      (end_fill)
+    )
+    (else
+      (platforms (+ t 0.01) y radius)
+    )
   )
 )
 
 (define (draw)
   (hideturtle)
-  (setposition -500 0)
-  (rt 90)
-  (fd 600)
   (pu)
-  (frame 0 -500 500 10 0 0 gravity elasticity colors -500 500)
-  (exitonclick)
+  (color "#ff9999")
+  (bgcolor "#191919")
+
+  (begin_fill)
+  (platforms 0 100 200)
+  (begin_fill)
+  (platforms 0 300 100)
+
+  (frame 0 0 450 0 0 450 1827394 0.002)
 )
 
 ; Please leave this last line alone.  You may add additional procedures above
